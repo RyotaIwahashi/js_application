f2274097591f9fbb20946c55de08daf8e9e86847
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-02-01 16:47:27 +0900

    ･ ログインによるtoken発行の実装

---
+++ backend/controllers/login.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  // トークンには、ユーザー名とユーザーIDがデジタル署名された形式で含まれている
+  // デジタル署名により、SECRET の値を知っている者だけが有効なトークンを生成できることが保証される
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
