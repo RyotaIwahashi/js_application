b50069bfc8f49df7a8a71f0f1f00648627dfa1eb
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-03-05 21:47:19 +0900

    storeを全コンポーネントで参照できるようにする

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import noteReducer from './reducers/noteReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(noteReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 
 // const root = ReactDOM.createRoot(document.getElementById('root'))
