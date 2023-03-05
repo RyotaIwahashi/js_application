3262917a06067fbd73664967e4c8b3453c3586ba
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-03-05 21:33:25 +0900

    concatではなくスプレッド構文を使用してみる

--- frontend/src/reducers/noteReducer.js
+++ frontend/src/reducers/noteReducer.js
@@ -1,7 +1,8 @@
 const noteReducer = (state = [], action) => {
   switch(action.type) {
   case 'NEW_NOTE':
-    return state.concat(action.payload)
+    // return state.concat(action.payload)
+    return [...state, action.payload]
   case 'TOGGLE_IMPORTANCE': {
     const id = action.payload.id
     const noteToChange = state.find(n => n.id === id)
